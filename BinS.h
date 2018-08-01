bee048ca39f36b3fd2ec0c9cfbecce6896ce1388
mHal <partyka.com@gmail.com>
2018-08-01 18:13:11 +0200

    Renaming file. Idea with #define instead of typedef will stay in my memory in shame file-organiser

--- BinS.h
+++ BinS.h
@@ -3,13 +3,13 @@
  *  
  *  @Brief: File contains deklarations of functions, that are used
  *          in BinS.c file
- *          Contains declaration of binSch type
+ *          Contains declaration of BINSCH type
  *          that type is default set by this file to int8_t
  *          under flag __BINSCH_TYPE__
  *          declared functions and array works on that type (check declar.)
  * 
  *  @Usage: If usege of type diff. than int8_t is required, then 
- *          type of elements in array, should be defined as binSch
+ *          type of elements in array, should be defined as BINSCH
  *          with preprocesor flag __BINSCH_TYPE__
  *          Function BinSearch_BinS should be used to binary search array
  *          Function BinarySearchInt8_Monotony can be used to test monotony
@@ -23,7 +23,7 @@
  * @Date: 31.07.2018
  **/
 
-#ifdef __BINSEARCH_INT8__
+#ifndef __BINSEARCH_INT8__
 #define __BINSEARCH_INT8__
 
 /*******************************************************************************
@@ -34,7 +34,7 @@
  *  @Brief: includs for different int types
  *  @Note: it could be done probably by one of them,
  *         given 2 for consistency with different compilers
- *  @TODO: check if one of (stdint or inttypes) is enough
+ *  @TODO: check if one of (stdint or inttypes) is enoug
  **/
 #include <stdint.h>
 #include <inttypes.h>
@@ -52,7 +52,7 @@
 /**
  *  @Brief: declaration of used in file type
  * 
- *  @Type: binSch - type that bin search will be done in
+ *  @Type: BINSCH - type that bin search will be done in
  * 
  **/
 
@@ -60,17 +60,29 @@
  *                              DEFINE
  ******************************************************************************/
 
-#ifndef __BINSCH_TYPE__
-    #define int8_t binSch;
-#endif  __BINSCH_TYPE__
+
+/**
+ *  @Brief: declaring new type for usage in binary search as default type - int8_t
+ *  @Usage: If different type is wanted to be used in binary search, then it have to be
+ *          typedef with name BinS_BINSCH
+ **/
+#if (BinS_BINSCH == FALSE)
+    typedef int8_t BinS_BINSCH;
+#else
+    typedef int64_t BinS_BINSCH;
+#endif /*__BinS_BINSCH_TYPE__*/
+
+#define int64_t lol
 
 #ifndef __DECRISING__
-    #define DECRISING 0U        /* by the exception that 0 means false */
+#define __DECRISING__
+    #define DECRISING (0U)        /* by the exception that 0 means false */
 #endif
 
 
 #ifndef __GROWING__
-    #define GROWING !DECRISING  /* logical negation of false is true which is DECRISING wanted value*/
+#define __GROWING__
+    #define GROWING (!DECRISING)  /* logical negation of false is true which is DECRISING wanted value*/
 #endif
 
 /*******************************************************************************
@@ -79,9 +91,9 @@
 
 typedef struct {
     uint32_t    index;
-    binSch      foundValue;
+    BinS_BINSCH foundValue;
     bool        found;
-} binarySearch_result;
+} BinS_result;
 
 
 /*******************************************************************************
@@ -92,15 +104,15 @@ typedef struct {
  * @Name: BinSearch_BinS - BinSearch is the name of the file.
  * @Brief: binary search req. that array is sorted inthis function it's unimpotant of order in which it is sorted
  *          !!! It is CRUCIAL to notice, that                     !!!
- *          !!! data type of array is definied as binSch typedef  !!!
- *          !!! under preprocesor flag__BINSCH_TYPE__             !!!
+ *          !!! data type of array is definied as BinS_BINSCH typedef  !!!
+ *          !!! under preprocesor flag__BinS_BINSCH_TYPE__             !!!
  *          which is default set to int8_tby this file
  * @Param1: length of array that is in Param2, the length of array have to be lower than 2^32
  * @Param2: array that we will do binary search on
  * @Param3: given value that we will search for
  * @Param4: pointer to function in which we could compare objects if returned type is traditional(int8_t,float,etc.)
  *          it is possible to set it to NULL, so standard compare would be make.
- *          Important to notice 1: comparator gets 2 arguments with type of binSch.
+ *          Important to notice 1: comparator gets 2 arguments with type of BinS_BINSCH.
  *          Important to notice 2: comparator return type of binarySearch_result
  * @Return: return structure wiich consist all important information from given search
  * 
@@ -108,14 +120,14 @@ typedef struct {
  * @Note: Array can be sorted in either growing or decrising secuences
  * 
  **/ 
-extern binarySearch_result BinSearch_BinS(  const uint32_t lengthOfArray,
-                                            binSch givenArray[ lengthOfArray ],
-                                            binSch wantedElement,
-                                            (void*)(*comparator)(binSch arg1, binSch arg2)
+extern BinS_result BinSearch_BinS(  const uint32_t lengthOfArray,
+                                            BinS_BINSCH givenArray[ lengthOfArray ],
+                                            BinS_BINSCH wantedElement,
+                                            BinS_result(*comparator)(BinS_BINSCH arg1, BinS_BINSCH arg2)
                                          );
 
 /**
- * @Name: BinarySearchInt8_Monotony - BinSearch is the name of the file.
+ * @Name: BinS_Monotony - BinSearch is the name of the file.
  * @Brief: Function evaluate if given array is sorted in growing, or decrising order
  * @Param1: length of array that is in Param2, the length of array have to be lower than 2^32
  * @Param2: array on witch we will do evaluation
@@ -125,6 +137,6 @@ extern binarySearch_result BinSearch_BinS(  const uint32_t lengthOfArray,
  * @Note: Array can be sorted in either growing or decrising secuences
  * 
  **/ 
-extern bool BinarySearchInt8_Monotony( const uint32_t lengthOfArray, binSch givenArray[ lengthOfArray ] );
+extern bool BinS_Monotony( const uint32_t lengthOfArray, BinS_BINSCH givenArray[ lengthOfArray ] );
 
 #endif /* __BINSEARCH_INT8__ */
